acb61df92800bd1c671f2dc237e9cbb4832de029
KyeNormanGill <kyenormangill@tafensw.net.au>
2017-11-12 21:59:31 +1100

    logs

---
+++ events/guildCreate.js
@@ -0,0 +1,11 @@
+const { RichEmbed } = require('discord.js');
+
+module.exports = function handle(guild) {
+	const embed = new RichEmbed()
+		.setColor(0x4fdd24)
+		.setThymbnail(guild.iconURL)
+		.setTitle(guild.name)
+		.setFooter('Joined guild')
+		.setTimestamp(new Date());
+	guild.client.channels.get('379222540155092993').send({ embed });
+};
---
+++ events/guildDelete.js
@@ -0,0 +1,11 @@
+const { RichEmbed } = require('discord.js');
+
+module.exports = function handle(guild) {
+	const embed = new RichEmbed()
+		.setColor(0xf93535)
+		.setThymbnail(guild.iconURL)
+		.setTitle(guild.name)
+		.setFooter('Left guild')
+		.setTimestamp(new Date());
+	guild.client.channels.get('379222540155092993').send({ embed });
+};
